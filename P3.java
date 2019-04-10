40c875bb02c9fad9e2a183ba948305f311007625
revatijaware <49470158+revatijaware@users.noreply.github.com>
2019-04-10 12:10:46 +0530

    Update P3.java

--- P3.java
+++ P3.java
@@ -14,7 +14,7 @@ class P3
 		System.out.println("a="+a);
 		System.out.println("b="+b);
 		System.out.println("");
-		System.out.println("a+b ="+(a+b));
+		System.out.println("a-b ="+(a-b));
 		System.out.println("a-b ="+(a-b));
 		System.out.println("a*b ="+(a*b));
 		System.out.println("a/b ="+(a/b));
